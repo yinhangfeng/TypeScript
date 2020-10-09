9ed39662817483a5beb043f0bd261cbe9b597b25
Sheetal Nandi <shkamat@microsoft.com>
2020-10-08 20:20:27 -0700

    Make getFilesByName buildinfo ready

--- src/compiler/types.ts
+++ src/compiler/types.ts
@@ -3833,6 +3833,10 @@ namespace ts {
     /*@internal*/
     export type FilePreprocessingDiagnostics = FilePreprocessingReferencedDiagnostic | FilePreprocessingFileExplainingDiagnostic;
 
+    /*@internal*/
+    export const missingSourceOfProjectReferenceRedirect = false;
+    /*@internal*/
+    export const missingFile = 0;
     export interface Program extends ScriptReferenceHost {
         getCurrentDirectory(): string;
         /**
@@ -3852,7 +3856,7 @@ namespace ts {
         /* @internal */
         getMissingFilePaths(): readonly Path[];
         /* @internal */
-        getFilesByNameMap(): ESMap<string, SourceFile | false | undefined>;
+        getFilesByNameMap(): ESMap<string, SourceFile | typeof missingSourceOfProjectReferenceRedirect | typeof missingFile>;
 
         /**
          * Emits the JavaScript and declaration files.  If targetSourceFile is not specified, then
